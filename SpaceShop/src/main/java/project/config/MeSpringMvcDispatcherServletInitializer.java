a6caa3b3b032335edbbf8455394c13486edb8a87
Elizaveta Antonova <lizaantonova1@yandex.ru>
2021-09-21 22:33:05 +0300

    new commit

---
+++ SpaceShop/src/main/java/project/config/MeSpringMvcDispatcherServletInitializer.java
@@ -0,0 +1,20 @@
+package project.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MeSpringMvcDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[] {SpringConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
+}
