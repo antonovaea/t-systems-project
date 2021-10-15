f509f64ade3301bdc37c3641e63ab5a2e9e29761
Elizaveta Antonova <lizaantonova1@yandex.ru>
2021-10-15 22:19:52 +0300

    change password works

--- src/main/java/project/spaceshop/controller/UserController.java
+++ src/main/java/project/spaceshop/controller/UserController.java
@@ -1,12 +1,14 @@
 package project.spaceshop.controller;
 
-import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import project.spaceshop.entity.User;
 import project.spaceshop.service.api.AddressService;
 import project.spaceshop.service.api.OrderService;
@@ -30,21 +32,20 @@ public class UserController extends CommonController {
     }
 
     @GetMapping(value = "/home/account")
-    public String accountInfo(Model model){
+    public String accountInfo(Model model) {
         model.addAttribute("user", userService.findUserFromSecurityContextHolder());
         return "account";
     }
 
     @GetMapping(value = "/home/account/password")
-    public String changePasswordForm(){
+    public String changePasswordForm() {
         return "passwordManager";
     }
 
     @PostMapping(value = "/home/account/change_password")
-    public String changePassword(Model model, User user, String oldPassword, String newPassword){
-        model.addAttribute("user", user = userService.findUserFromSecurityContextHolder());
-        model.addAttribute("oldPassword", oldPassword = user.getPassword());
-        model.addAttribute("newPassword", newPassword);
+    @PreAuthorize("hasAuthority('USER')")
+    public String changePassword(@RequestParam(name = "oldPassword") String oldPassword, @RequestParam(name = "newPassword") String newPassword) {
+        User user = userService.findUserByEmail(SecurityContextHolder.getContext().getAuthentication().getName());
         userService.changePassword(oldPassword, newPassword);
         return "/success";
     }
